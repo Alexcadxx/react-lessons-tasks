f56b889f5a4ea37c705d96e0440d1dfc2763847d
Alexcad <alexcadxx@gmail.com>
2025-10-30 12:54:34 +0300

    Created To Do List by data from Json Placeholder with todos endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from 'react';
+
 import styles from './app.module.css';
+import { Todo } from './components';
 
 export function App() {
+	const [todos, setTodos] = useState([]);
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then((todosData) => todosData.json())
+			.then((loadedTodos) => setTodos(loadedTodos));
+	}, []);
+
 	return (
-		<>
-			<div className={styles.app}>START</div>
-		</>
+		<div className={styles.app}>
+			<div>
+				{todos.map(({ id, title, completed }) => (
+					<Todo key={id} title={title} completed={completed} />
+				))}
+			</div>
+		</div>
 	);
 }
